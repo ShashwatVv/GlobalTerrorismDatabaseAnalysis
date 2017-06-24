907e7294e85e11c49ce05dda4ec9691aa7574f5d
Uros Stegic <urosstegic@gmx.com>
2017-06-24 16:43:26 +0200

    Finished DBSCAN process.

--- code/cluster.py
+++ code/cluster.py
@@ -4,52 +4,45 @@ import sklearn.metrics as met
 from sklearn import preprocessing
 from sklearn.cluster import DBSCAN
 
+preprocess = 'std'
+
+print('citanje podataka...', end='')
 df = pd.read_csv("datasets/backup2_preimenovano.csv")
-
-#prikaz imena kolona + 5 prvih instanci
-print('Prvih 5 instanci', df.head(), sep='\n')
-print('\n\n')
-
+print('gotovo')
 features = df.columns[1:12].tolist()
 x_original=df[features]
 
-#bez preprocesiranja
-#x = x_original
+print('Preprocesiranje podataka...', end='')
+# Preprocesiranje
+if preprocess == 'std':
+    x=pd.DataFrame(preprocessing.scale(x_original))
+elif preprocess == 'norm':
+    x=pd.DataFrame(preprocessing.MinMaxScaler().fit_transform(x_original))
+else:
+    x = x_original
+print('gotovo')
 
-#standardizacija atributa
-x=pd.DataFrame(preprocessing.scale(x_original))
-
-#normalizacija
-#x=pd.DataFrame(preprocessing.MinMaxScaler().fit_transform(x_original))
-
-#dodeljivanje imena kolonama
+# Dodeljivanje imena kolonama
 x.columns = features
 
-"""
-Density-Based Spatial Clustering of Applications with Noise
+print('features:', features)
 
-parametri:
-
-eps : maksimalna udaljenost za instance-komsije
-      default:0.5
-min_samples : broj suseda koje mora da ima instanci u jezgru
-      default:5
-metric : metrika za racunanje rastojanja
-      default:'euclidean'
-
-atributi:
-
-core_sample_indices_ : indeksi instanci u jezgru
-
-components_ : kopija instanci u jezgru
-labels_ : oznake klastera za svaku instancu. Instance koje se smatraju sumom imaju
-oznaku -1
-"""
-
-for eps in range(3, 8):
-    for min_samples in range(2, 4):
+print('=-----------------------------------------=')
+print('=              klasterovanje              =')
+print('=-----------------------------------------=')
+for eps in range(1, 21):
+    for min_samples in range(0, 201, 10):
+        if min_samples == 0:
+            min_samples = min_samples + 1
+        print("eps:", eps)
+        print("min_samples:", min_samples)
         est=DBSCAN(eps=eps*0.1, min_samples=min_samples)
         est.fit(x)
-        df['labels']= est.labels_
+        col_name = 'labels_' + str(eps) + '_' + str(min_samples)
+        df[col_name]= est.labels_
         num_clusters=max(est.labels_)+1
+        print("number of clusters:", num_clusters)
+        print('=-----------------------------------------=')
+
+df.to_csv('datasets/clustered_data.csv')
 
